bd5fba910f78a9e818b05211a044e50e26b80c68
карина <@DESKTOP-FP9I0KA>
2024-09-10 13:49:13 +0300

    Add project files.

---
+++ Grains/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Grains
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            ulong totalGrains = 0; 
+            ulong grainsOnSquare = 1; 
+
+           
+            for (int square = 1; square <= 64; square++)
+            {
+                Console.WriteLine($"Клiтинка {square}: {grainsOnSquare} зерен");
+                totalGrains += grainsOnSquare;
+                grainsOnSquare *= 2;
+            }
+
+            Console.WriteLine($"\nЗагальна кiлькiсть зерен на шахiвницi: {totalGrains}");
+        }
+    }
+}
